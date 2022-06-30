61032a3e2bb2881ab603af29e6b924b20d8b93ea
Polina Mikheeva <mikheeva_polli@mail.ru>
2022-06-30 22:38:27 +0300

    completed task1

--- Task1/app/src/main/java/com/example/task1/MainActivity.kt
+++ Task1/app/src/main/java/com/example/task1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
@@ -11,17 +12,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun main() {
+    fun main(view: View) {
 
-        val name = findViewById<TextView>(R.id.Name)
-        val height = findViewById<TextView>(R.id.Height)
-        val weight = findViewById<TextView>(R.id.Weight)
-        val age = findViewById<TextView>(R.id.Age)
-        var salary = (Integer.parseInt(height.text.toString()) + Integer.parseInt(weight.text.toString())) * Integer.parseInt(age.text.toString()) * Integer.parseInt(name.length().toString())
+        val name = Integer.parseInt(findViewById<TextView>(R.id.Name).length().toString())
+        val height = Integer.parseInt(findViewById<TextView>(R.id.Height).text.toString())
+        val weight = Integer.parseInt(findViewById<TextView>(R.id.Weight).text.toString())
+        val age = Integer.parseInt(findViewById<TextView>(R.id.Age).text.toString())
         val btn = findViewById<Button>(R.id.button)
-        btn.setOnClickListener(){
-            var result = findViewById<TextView>(R.id.Salary)
-            result.setText(Integer.toString(salary));
+        if (name in 1..50) {
+            if (height in 1..250) {
+                if (weight in 1..250) {
+                    if (age in 1..150) {
+                        var salary = (height + weight) * age * name
+                        btn.setOnClickListener(){
+                            var result = findViewById<TextView>(R.id.Salary)
+                            result.setText(Integer.toString(salary))
+                        }
+                    } else {
+                        btn.setOnClickListener(){
+                            var result = findViewById<TextView>(R.id.Salary)
+                            result.setText("Данные введены некорректно")
+                        }
+                    }
+                } else {
+                    btn.setOnClickListener(){
+                        var result = findViewById<TextView>(R.id.Salary)
+                        result.setText("Данные введены некорректно")
+                    }
+                }
+            } else {
+                btn.setOnClickListener(){
+                    var result = findViewById<TextView>(R.id.Salary)
+                    result.setText("Данные введены некорректно")
+                }
+            }
+        } else {
+            btn.setOnClickListener(){
+                var result = findViewById<TextView>(R.id.Salary)
+                result.setText("Данные введены некорректно")
+            }
         }
+
     }
 }
