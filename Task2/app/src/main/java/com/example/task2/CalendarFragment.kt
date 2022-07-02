b12c546e0138935235a961ad1e54d7b8539f4fdf
Polina Mikheeva <mikheeva_polli@mail.ru>
2022-07-02 19:51:10 +0300

    task2 with fragments

---
+++ Task2/app/src/main/java/com/example/task2/CalendarFragment.kt
@@ -0,0 +1,6 @@
+package com.example.task2
+
+import androidx.fragment.app.Fragment
+
+class CalendarFragment : Fragment(R.layout.fragment_calendar) {
+}
---
+++ Task2/app/src/main/java/com/example/task2/PhotoFragment.kt
@@ -0,0 +1,6 @@
+package com.example.task2
+
+import androidx.fragment.app.Fragment
+
+class PhotoFragment : Fragment(R.layout.fragment_photo) {
+}
