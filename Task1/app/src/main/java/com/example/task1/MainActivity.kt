071dffe6489daba053eb638dda742b8a040ccf9c
Polina Mikheeva <mikheeva_polli@mail.ru>
2022-07-02 11:16:38 +0300

    new checking

--- Task1/app/src/main/java/com/example/task1/MainActivity.kt
+++ Task1/app/src/main/java/com/example/task1/MainActivity.kt
@@ -12,24 +12,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun main(view: View) {
+    val n = findViewById<TextView>(R.id.Name).text.toString()
+    val h = findViewById<TextView>(R.id.Height).text.toString()
+    val w = findViewById<TextView>(R.id.Weight).text.toString()
+    val a = findViewById<TextView>(R.id.Age).text.toString()
+    val btn = findViewById<Button>(R.id.button)
 
-        val h = findViewById<TextView>(R.id.Height).text.toString()
-        val w = findViewById<TextView>(R.id.Weight).text.toString()
-        val a = findViewById<TextView>(R.id.Age).text.toString()
-
-        val name = findViewById<TextView>(R.id.Name).text.toString()
-        val height = Integer.parseInt(h)
-        val weight = w.toDouble()
-        val age = Integer.parseInt(a)
-        val btn = findViewById<Button>(R.id.button)
-
-        if (name.isEmpty() || h.isEmpty() || w.isEmpty() || a.isEmpty()) {
+    fun emptyCheck() {
+        if (n.isEmpty() || h.isEmpty() || w.isEmpty() || a.isEmpty()) {
             btn.setOnClickListener() {
                 var result = findViewById<TextView>(R.id.Salary)
                 result.text = "Данные не введены"
             }
-        } else if (name.isNotEmpty() && height in 1..250 && (weight > 0 && weight < 250) && age in 1..150) {
+        }
+    }
+
+    fun main(view: View) {
+
+        emptyCheck()
+
+        val name = n
+        val height = h.toInt()
+        val weight = w.toDouble()
+        val age = a.toInt()
+
+        if (height in 1..250 && (weight > 0 && weight < 250) && age in 1..150) {
             var salary = (height + weight) * age * name.length
             btn.setOnClickListener(){
                 var result = findViewById<TextView>(R.id.Salary)
